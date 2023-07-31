6f0f102a44a4775d3e094bbaa6f52dbcde96ec84
carlton <64127681+notcarlton@users.noreply.github.com>
2023-07-31 16:42:23 -0600

    goodbye harmony

--- Unfair/Loader.cs
+++ Unfair/Loader.cs
@@ -1,31 +1,21 @@
-﻿using HarmonyLib;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Unfair
 {
     public static class Loader
     {
         private static GameObject _gameObject;
-        private static Main _main;
 
         public static void Load()
         {
             _gameObject = new GameObject();
-            _main = _gameObject.AddComponent<Main>();
+            _gameObject.AddComponent<Main>();
             Object.DontDestroyOnLoad(_gameObject);
-            
-            // initialize harmony and patch
-            Harmony.DEBUG = true;
-            _main.Harmony = new HarmonyLib.Harmony("unfair");
-            _main.Harmony.PatchAll();
         }
 
         public static void Unload()
         {
-	        // unpatch
-	        _main.Harmony.UnpatchAll();
-	        
-            Object.Destroy(_gameObject);
+	        Object.Destroy(_gameObject);
         }
     }
 }

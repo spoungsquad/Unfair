b2c9889d250d1da4a7179c26b5dccbea73ae549b
carlton <64127681+notcarlton@users.noreply.github.com>
2023-07-31 12:04:38 -0600

    private access

---
+++ Unfair/Util/PrivateAccess.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Unfair.Util
+{
+	public static class PrivateAccess
+	{
+		public static void SetPrivateProperty<T>(T obj, string propertyName, object newValue)
+		{
+			foreach (var fi in obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+			{
+				if (!fi.Name.ToLower().Contains(propertyName.ToLower())) continue;
+				
+				fi.SetValue(obj, newValue);
+				break;
+			}
+		}
+
+		// wtf
+		public static object GetPrivateProperty<T>(T obj, string propertyName)
+		{
+			return (from fi in obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic) 
+				where fi.Name.ToLower().Contains(propertyName.ToLower()) 
+				select fi.GetValue(obj))
+				.FirstOrDefault();
+		}
+
+		public static object CallPrivateMethod<T>(T obj, string methodName, object[] param)
+		{
+			var mi = obj.GetType().GetMethod(methodName);
+			return mi != null ? mi.Invoke(obj, param) : null;
+		}
+	}
+}

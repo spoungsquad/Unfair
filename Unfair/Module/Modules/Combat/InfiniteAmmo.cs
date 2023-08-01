3d986ed2e9bacc7c21df2542482b61677d4dddba
Flash <stop@doxxing.me>
2023-07-31 21:45:50 -0500

    Infinite Ammo

---
+++ Unfair/Module/Modules/Combat/InfiniteAmmo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Unfair.Module.Modules.Combat
+{
+    public class InfiniteAmmo : Module
+    {
+        public InfiniteAmmo() : base("InfiniteAmmo", "Infinite ammo", Category.Combat, KeyCode.None)
+        {
+            Enabled = true;
+        }
+
+        public override void OnUpdate()
+        {
+            PlayerController.LHFJFKJJKCG.NLJCMEMPBLA.GBNMBOFAHAA.SetCurrentMagazineAmount(10000);
+        }
+    }
+}

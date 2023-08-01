2c3e41411595bbdd1c17c4360cc7c6e34e3dde87
imAETHER <36291026+imAETHER@users.noreply.github.com>
2023-07-31 23:25:49 -0300

    godmode

---
+++ Unfair/Module/Modules/Player/GodMode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Unfair.Module.Modules.Player
+{
+    public class GodMode : Module
+    {
+        public GodMode() : base("GodMode", "Be invincible to all damage", Category.Player, KeyCode.F9)
+        {
+        }
+
+        public override void OnEnable()
+        {
+            PlayerController.LHFJFKJJKCG.JDHLKGBHMAD.SetPlayerImmunity(true);
+        }
+
+        public override void OnDisable()
+        {
+            PlayerController.LHFJFKJJKCG.JDHLKGBHMAD.SetPlayerImmunity(false);
+        }
+    }
+}

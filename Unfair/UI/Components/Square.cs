86daa1de5056b505c5a48556c4b3836bd23073fb
carlton <64127681+notcarlton@users.noreply.github.com>
2023-08-01 09:53:42 -0600

    can someone try this idk if it works

---
+++ Unfair/UI/Components/Square.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unfair.UI.Components
+{
+	public class Square : Menu.MenuComponent
+	{
+		protected override void OnPopulateMesh(VertexHelper vh)
+		{
+			vh.Clear();
+			
+			// i have to write vertices manually. my blood will be on the floor today
+			var rect = rectTransform.rect;
+			var w = rect.width;
+			var h = rect.height;
+
+			var vert = UIVertex.simpleVert;
+			vert.color = color;
+			
+			vert.position = new Vector2(0, 0);
+			vh.AddVert(vert);
+			
+			vert.position = new Vector2(0, h);
+			vh.AddVert(vert);
+			
+			vert.position = new Vector2(w, h);
+			vh.AddVert(vert);
+			
+			vert.position = new Vector2(w, 0);
+			vh.AddVert(vert);
+			
+			// triangles
+			vh.AddTriangle(0, 1, 2);
+			vh.AddTriangle(2, 3, 0);
+		}
+	}
+}
---
+++ Unfair/UI/Menu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unfair.UI
+{
+	public class Menu
+	{
+		public class MenuComponent : Graphic
+		{
+			public MenuComponent Add<T>() where T : MenuComponent
+			{
+				var obj = new GameObject();
+				obj.AddComponent<T>();
+				obj.transform.SetParent(transform);
+				
+				return this;
+			}
+		}
+		
+		private GameObject _gameObject;
+		
+		// components
+		private Canvas _canvas;
+		private CanvasScaler _canvasScaler;
+		private GraphicRaycaster _graphicRaycaster;
+		
+		public Menu()
+		{
+			_gameObject = new GameObject();
+			Object.DontDestroyOnLoad(_gameObject); // so our UI doesn't kill itself when we load into a new scene
+			
+			// add components
+			_canvas = _gameObject.AddComponent<Canvas>(); // will also create a RectTransform
+			_canvasScaler = _gameObject.AddComponent<CanvasScaler>();
+			_graphicRaycaster = _gameObject.AddComponent<GraphicRaycaster>();
+			
+			// config stuff
+			_canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+			_canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+		}
+		
+		// add a component to the menu
+		public Menu Add<T>() where T : MenuComponent
+		{
+			var obj = new GameObject();
+			obj.AddComponent<T>();
+			obj.transform.SetParent(_gameObject.transform);
+			
+			return this;
+		}
+	}
+}

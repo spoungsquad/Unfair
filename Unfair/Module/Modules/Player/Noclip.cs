bfea5c61d7880daeb0ade64ea9a85cf57b7b4e7d
carlton <64127681+notcarlton@users.noreply.github.com>
2023-07-31 19:45:47 -0600

    noclip

---
+++ Unfair/Module/Modules/Player/Noclip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Unfair.Module.Modules.Player
+{
+	public class Noclip : Module
+	{
+		public Noclip() : base("Noclip", "Disables player collision", Category.Player, KeyCode.G)
+		{
+		}
+		
+		public override void OnUpdate()
+		{
+			PlayerController.LHFJFKJJKCG.ICCJNHOFBGO.enabled = false;
+		}
+		
+		public override void OnDisable()
+		{
+			PlayerController.LHFJFKJJKCG.ICCJNHOFBGO.enabled = true;
+		}
+	}
+}

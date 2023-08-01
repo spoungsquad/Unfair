bd736018208137ddf442a8aecf2c056355d7d39b
Flash <stop@doxxing.me>
2023-07-31 21:27:42 -0500

    aether fix

--- Unfair/Module/Modules/Movement/Spinbot.cs
+++ Unfair/Module/Modules/Movement/Spinbot.cs
@@ -13,9 +13,14 @@ namespace Unfair.Module.Modules.Movement
         public override void OnUpdate()
         {
             i++;
-            PlayerController.LHFJFKJJKCG.gameObject.transform.Rotate(0, i, 0);
+            PlayerController.LHFJFKJJKCG.gameObject.transform.Rotate(i, i, i);
             if (i > 360)
                 i = 0;
+        }
+        
+        public override void OnDisable()
+        {
+            PlayerController.LHFJFKJJKCG.gameObject.transform.rotation = Quaternion.identity;
         }
     }
 }

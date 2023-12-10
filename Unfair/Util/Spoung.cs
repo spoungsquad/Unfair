6d64146c519f5e77a6ba8c1a88b19b3c51d5174f
carlton <64127681+notcarlton@users.noreply.github.com>
2023-12-10 11:50:29 -0700

    spoung !

---
+++ Unfair/Util/Spoung.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Unfair.Util
+{
+	public static class Spoung
+	{
+		public static Texture2D DeployTheSpoung()
+		{
+			using (var stream = typeof(Spoung).Assembly.GetManifestResourceStream("Unfair.Resources.spoung.png"))
+			{
+				if (stream == null)
+				{
+					DebugConsole.Write("spoung.png not found in resources");
+					return null;
+				}
+				
+				var bytes = new byte[stream.Length];
+				var read = stream.Read(bytes, 0, bytes.Length);
+				
+				if (read != bytes.Length)
+				{
+					DebugConsole.Write("spoung.png not read correctly");
+					return null;
+				}
+
+				var texture = new Texture2D(1, 1); // not actual size
+				texture.LoadImage(bytes);
+				return texture;
+			}
+		}
+	}
+}

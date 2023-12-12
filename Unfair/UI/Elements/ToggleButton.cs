fc734b0b790123cbabbffd21237f208beaad047a
carlton <64127681+notcarlton@users.noreply.github.com>
2023-12-12 08:44:53 -0700

    better toggle button i think

--- Unfair/UI/Elements/ToggleButton.cs
+++ Unfair/UI/Elements/ToggleButton.cs
@@ -1,11 +1,38 @@
-﻿namespace Unfair.UI.Elements
+﻿using System;
+using Unfair.Util;
+using UnityEngine;
+
+namespace Unfair.UI.Elements
 {
-    public class ToggleButton : Button
+    public class ToggleButton : UIElement
     {
+        public Color Color;
+        public Color ToggleColor;
+        public Color TextColor;
+        public Color StrokeColor;
+        public float StrokeWidth;
+        public string Text;
+        public bool IsPressed;
+        public bool IsCentered;
+        public Action<ToggleButton> OnClick;
         public bool IsToggled;
         
         public override void Draw()
         {
+            Vector2 position = AdjustedPosition();
+            
+            var color = IsToggled ? ToggleColor : Color;
+            
+            Render.FillRect(position, Rect.size, color);
+            Render.DrawRect(position, Rect.size, StrokeColor, StrokeWidth);
+            Render.DrawString(position, Text, TextColor, IsCentered);
+            
+            IsPressed = GUI.Button(new Rect(position, Rect.size), new GUIContent(""), GUI.skin.label);
+            if (IsPressed && OnClick != null)
+            {
+                OnClick(this);
+            }
+            
             if (IsPressed)
                 IsToggled = !IsToggled;
             
